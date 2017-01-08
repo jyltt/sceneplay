c6c8b6971d307a7b0dda4965d3155fefffd3fbe1
wuming <358591708@qq.com>
2017-01-09 00:26:04 +0800

    up

--- Sceneplay/Form1.Designer.cs
+++ Sceneplay/Form1.Designer.cs
@@ -28,12 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SceneTree = new System.Windows.Forms.TreeView();
+            this.funcList = new System.Windows.Forms.ComboBox();
+            this.param = new System.Windows.Forms.RichTextBox();
+            this.paramType = new System.Windows.Forms.ListBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SceneTree
+            // 
+            this.SceneTree.Dock = System.Windows.Forms.DockStyle.Left;
+            this.SceneTree.ItemHeight = 14;
+            this.SceneTree.LineColor = System.Drawing.Color.Bisque;
+            this.SceneTree.Location = new System.Drawing.Point(0, 0);
+            this.SceneTree.Name = "SceneTree";
+            this.SceneTree.PathSeparator = ".";
+            this.SceneTree.Size = new System.Drawing.Size(146, 491);
+            this.SceneTree.TabIndex = 0;
+            this.SceneTree.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.SceneTreeClickItem);
+            // 
+            // funcList
+            // 
+            this.funcList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.funcList.FormattingEnabled = true;
+            this.funcList.Location = new System.Drawing.Point(152, 12);
+            this.funcList.Name = "funcList";
+            this.funcList.Size = new System.Drawing.Size(121, 20);
+            this.funcList.TabIndex = 1;
+            // 
+            // param
+            // 
+            this.param.Location = new System.Drawing.Point(297, 48);
+            this.param.Name = "param";
+            this.param.Size = new System.Drawing.Size(120, 398);
+            this.param.TabIndex = 3;
+            this.param.Text = "";
+            this.param.WordWrap = false;
+            // 
+            // paramType
+            // 
+            this.paramType.FormattingEnabled = true;
+            this.paramType.ItemHeight = 12;
+            this.paramType.Location = new System.Drawing.Point(153, 48);
+            this.paramType.Name = "paramType";
+            this.paramType.Size = new System.Drawing.Size(120, 436);
+            this.paramType.TabIndex = 4;
+            this.paramType.SelectedIndexChanged += new System.EventHandler(this.paramType_SelectedIndexChanged);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnSave.Location = new System.Drawing.Point(363, 461);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(54, 23);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "保存";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnDelet_Click);
+            // 
+            // btnDelet
+            // 
+            this.btnDelet.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnDelet.Location = new System.Drawing.Point(297, 461);
+            this.btnDelet.Name = "btnDelet";
+            this.btnDelet.Size = new System.Drawing.Size(54, 23);
+            this.btnDelet.TabIndex = 5;
+            this.btnDelet.Text = " 删除";
+            this.btnDelet.UseVisualStyleBackColor = true;
+            this.btnDelet.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 491);
+            this.Controls.Add(this.btnDelet);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.paramType);
+            this.Controls.Add(this.param);
+            this.Controls.Add(this.funcList);
+            this.Controls.Add(this.SceneTree);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TreeView SceneTree;
+        private System.Windows.Forms.ComboBox funcList;
+        private System.Windows.Forms.RichTextBox param;
+        private System.Windows.Forms.ListBox paramType;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelet;
     }
 }
 
--- Sceneplay/Form1.cs
+++ Sceneplay/Form1.cs
@@ -8,13 +8,189 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Sceneplay
 {
     public partial class Form1 : Form
     {
+        // 配置文件信息
+        Dictionary<int, List<int>> m_FileInfo = new Dictionary<int, List<int>>(); // hurdleid sceneplayid
+        Dictionary<int, List<string>> m_SceneplayInfo = new Dictionary<int, List<string>>(); // sceneplayid funcname
+        Dictionary<string, Dictionary<string, string>> m_FuncInfo = new Dictionary<string, Dictionary<string, string>>();//sceneplayid+funcname+index list<type, value>
+        // 相应函数信息
+        Dictionary<string, List<string>> m_FuncCfg = new Dictionary<string, List<string>>();// funcname paramList
+
+        // 
+        TreeNode m_curNode;
+
         public Form1()
         {
             InitializeComponent();
+            ReadFuncListFile();
+            ReadCfgFile();
+            CreateTree();
+        }
+
+        private void ReadCfgFile()
+        {
+            var list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            m_FileInfo.Add(1000, list);
+            var list2 = new List<string>();
+            list2.Add("play");
+            list2.Add("play");
+            var list3 = new List<string>();
+            list3.Add("play");
+            list3.Add("play");
+            m_SceneplayInfo.Add(1, list2);
+            m_SceneplayInfo.Add(2, list3);
+
+            var dic = new Dictionary<string, string>();
+            dic.Add("param1", "0");
+            dic.Add("param2", "1");
+            m_FuncInfo.Add("1play1", dic);
+            var dic1 = new Dictionary<string, string>();
+            dic1.Add("param1", "2");
+            dic1.Add("param2", "3");
+            m_FuncInfo.Add("1play2", dic1);
+            var dic2 = new Dictionary<string, string>();
+            dic2.Add("param1", "4");
+            dic2.Add("param2", "5");
+            m_FuncInfo.Add("2play1", dic2);
+            var dic3 = new Dictionary<string, string>();
+            dic3.Add("param1", "6");
+            dic3.Add("param2", "7");
+            m_FuncInfo.Add("2play2", dic3);
+        }
+
+        private void ReadFuncListFile()
+        {
+            var list = new List<string>();
+            list.Add("param1");
+            list.Add("param2");
+            m_FuncCfg.Add("play",list);
+            var list1 = new List<string>();
+            list1.Add("p1");
+            list1.Add("p2");
+            m_FuncCfg.Add("play2",list1);
+        }
+
+        private void CreateFuncList()
+        {
+            ClearFuncList();
+            foreach(var dic in m_FuncCfg)
+            {
+                funcList.Items.Add(dic.Key.ToString());
+            }
+        }
+
+        private void ClearFuncList()
+        {
+            funcList.Items.Clear();
+            ClearParamTypeList();
+        }
+
+        private void ClearParamTypeList()
+        {
+            paramType.Items.Clear();
+        }
+
+        private void CreateTree()
+        {
+            foreach(var dic in m_FileInfo)
+            {
+                TreeNode nodeHurdle = new TreeNode(dic.Key.ToString());
+                foreach(var sceneplay_id in dic.Value)
+                {
+                    TreeNode nodeSceneplay = new TreeNode(sceneplay_id.ToString());
+                    if (m_SceneplayInfo.ContainsKey(sceneplay_id))
+                    {
+                        foreach (var func in m_SceneplayInfo[sceneplay_id])
+                        {
+                            TreeNode nodeFuncName = new TreeNode(func);
+                            nodeSceneplay.Nodes.Add(nodeFuncName);
+                        }
+                    }
+                    nodeSceneplay.Nodes.Add(new TreeNode("+"));
+                    nodeHurdle.Nodes.Add(nodeSceneplay);
+                }
+                //nodeHurdle.Nodes.Add(new TreeNode("+"));
+                SceneTree.Nodes.Add(nodeHurdle);
+            }
+            //SceneTree.Nodes.Add(new TreeNode("+"));
+        }
+
+        private void CreateParamTypeList(int sceneplayId, string funcName, int index)
+        {
+            ClearParamTypeList();
+            if (!m_FuncCfg.ContainsKey(funcName))
+                return;
+            foreach (var type in m_FuncCfg[funcName])
+            {
+                ComboBoxItem item = new ComboBoxItem();
+                item.Text = type;
+                var value = new List<string>();
+                value.Add(sceneplayId.ToString());
+                value.Add(funcName);
+                value.Add((index+1).ToString());
+                item.Value = value;
+                paramType.Items.Add(item);
+            }
+        }
+
+        private void SetParam(string funcName, string p)
+        {
+            param.Text = m_FuncInfo[funcName][p];
+        }
+
+        private void SelectFunc(int hurdleId, int sceneplayId, string funcName, int index)
+        {
+            int id = funcList.Items.IndexOf(funcName);
+            funcList.SelectedIndex = id;
+            CreateParamTypeList(sceneplayId, funcName, index);
+        }
+
+        ///////////////////////////////////////////////////////////////
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void SceneTreeClickItem(object sender, TreeViewEventArgs e)
+        {
+            m_curNode = e.Node;
+            var index = e.Node.Index;
+            string nodeName = e.Node.Text;
+            string fullPath = e.Node.FullPath;
+            string[] nodeParent = fullPath.Split('.');
+            if (nodeParent.Length == 3)
+            {
+                CreateFuncList();
+                int hurdleId = System.Int32.Parse(nodeParent[0]);
+                int sceneplayId = System.Int32.Parse(nodeParent[1]);
+                string funcName = nodeParent[2];
+                SelectFunc(hurdleId, sceneplayId, funcName, index);
+            }
+            else
+            {
+                ClearFuncList();
+            }
+        }
+
+        private void btnDelet_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void paramType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var item = (ComboBoxItem)paramType.SelectedItem;
+            var funcInfo = (List<string>)item.Value;
+            var sceneplayId = funcInfo[0];
+            var funcName = funcInfo[1];
+            var funcIndex = funcInfo[2];
+            SetParam(sceneplayId+funcName + funcIndex, item.Text);
         }
     }
 }
