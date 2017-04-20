cee724257547628feab00e017bef94001d05f645
wuming <358591708@qq.com>
2017-04-20 23:35:13 +0800

    up

--- Sceneplay/ui/ActionFuncUI.cs
+++ Sceneplay/ui/ActionFuncUI.cs
@@ -20,7 +20,25 @@ namespace Sceneplay.ui
             m_curScreenplayID = screenplay_id;
             m_curFuncIndex = index;
             InitializeComponent();
+            CreateParamTypeList();
+        }
+
+        private void CreateParamTypeList()
+        {
+            m_listParamType.Items.Clear();
             var _funcInfo = FileManager.GetInstance().ContentMgr.GetFuncInfo(m_curScreenplayID, m_curFuncIndex);
+            if (_funcInfo == null)
+                return;
+            var func = _funcInfo.ActInfo as FuncInfo;
+            var funcInfo = FileManager.GetInstance().FuncCfgMgr.GetFuncCfg(func.Name);
+            foreach (var type in func.GetParamType())
+            {
+                var param = funcInfo.GetParamInfo(type);
+                BoxItem item = new BoxItem();
+                item.Text = type + param.DescribeSimple;
+                item.Value = type;
+                m_listParamType.Items.Add(item);
+            }
         }
 
         private void m_listParamType_SelectedIndexChanged(object sender, EventArgs e)
